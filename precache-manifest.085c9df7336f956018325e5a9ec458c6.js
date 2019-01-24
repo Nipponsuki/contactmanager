b78d8016b090c83d8a1f6d26661a5cd5cf4e95a1
Alex <alex.alexeev23061991@gmail.com>
2019-01-24 21:47:47 +0300

    Updates

---
+++ precache-manifest.085c9df7336f956018325e5a9ec458c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40383d76dacdb6955ff",
+    "url": "/Nipponsuki/contactmanager/static/js/runtime~main.d40383d7.js"
+  },
+  {
+    "revision": "248ce8e45875ba45e9ae",
+    "url": "/Nipponsuki/contactmanager/static/js/main.248ce8e4.chunk.js"
+  },
+  {
+    "revision": "3cf0ccd00993780ae0b2",
+    "url": "/Nipponsuki/contactmanager/static/js/1.3cf0ccd0.chunk.js"
+  },
+  {
+    "revision": "248ce8e45875ba45e9ae",
+    "url": "/Nipponsuki/contactmanager/static/css/main.4ab803a3.chunk.css"
+  },
+  {
+    "revision": "3cf0ccd00993780ae0b2",
+    "url": "/Nipponsuki/contactmanager/static/css/1.f7aff8b2.chunk.css"
+  },
+  {
+    "revision": "4cb4a24b3a2c65e97ef1a2c5e00ec512",
+    "url": "/Nipponsuki/contactmanager/index.html"
+  }
+];
